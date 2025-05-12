e49f8e6560f1309183cf4f7478be4fb1259439c3
Shakawat Rasel <shakawat.rasel001@stu.bmcc.cuny.edu>
2025-05-12 19:33:45 -0400

    Initial commit: HTTPS server with dummy certs

---
+++ server.js
@@ -0,0 +1,18 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, HTTPS world!');
+});
+
+const options = {
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('fullchain.pem')
+};
+
+https.createServer(options, app).listen(3000, () => {
+  console.log('Secure server running at https://localhost:3000');
+});
